000766a92826479a8ca46ddbe0189e1fc2e0bed6
Nijhumtara <nijhumtara1311@gmail.com>
2026-05-18 17:12:45 +0600

    Add Email Integration Feature

--- port.js
+++ port.js
@@ -15,3 +15,14 @@ const observer = new IntersectionObserver((entries) => {
 }, { threshold: 0.1 });
 
 document.querySelectorAll('.section, .project, .skill').forEach(el => observer.observe(el));
+
+function sendMail(){
+  let params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  }
+
+  emailjs.send("service_8rprlfb", "template_veds9tj", params).then(alert("Email sent successfully!!"))
+}
